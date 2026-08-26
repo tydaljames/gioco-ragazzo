1c835b1cffb7f2464f8f91e1ccfc462604cbdb08
tydaljames <tydaljames@gmail.com>
2026-08-25 20:43:46 -0700

    Added MMU

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod mmu;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/mmu.rs
@@ -0,0 +1,98 @@
+use std::fs;
+use std::path::Path;
+
+pub struct Cartridge {
+    pub rom: Vec<u8>
+}
+
+impl Cartridge {
+    // This function will read a file from the computer
+    // and return a Result. If successful, gives us a Cartridge
+    pub fn load<P: AsRef<Path>>(path: P) -> Result<Self, std::io::Error> {
+        let rom = fs::read(path)?;
+        Ok(Self { rom })
+    }
+}
+
+pub struct Mmu {
+    cartridge: Cartridge,
+    vram: [u8; 0x2000], // 8,196 bytes/ 8 KB video RAM
+    wram: [u8; 0x2000], // 8 KB working RAM
+    hram: [u8; 0x7F], // 127 bytes of high-ram (fast CPU operations)
+    ie_register: u8, // Single byte for interrupts
+}
+
+impl Mmu {
+    // Constructor to create new MMU and initialize arrays to 0
+    pub fn new(cartridge: Cartridge) -> Self {
+        Self {
+            cartridge,
+            vram: [0; 0x2000],
+            wram: [0; 0x2000],
+            hram: [0; 0x7F],
+            ie_register: 0,
+        }
+    }
+
+    pub fn read_byte(&self, addr: u16) -> u8 {
+        match addr {
+            0x0000..=0x7FFF => self.cartridge.rom[addr as usize],
+            0x8000..=0x9FFF => {
+                let index = (addr - 0x8000) as usize;
+                self.vram[index]
+            }
+
+            // Remove this?
+            0xC000..=0xDFFF => {
+                let index = (addr - 0xC000) as usize;
+                self.wram[index]
+            }
+            0xFF80..=0xFFFE => {
+                let index = (addr - 0xFF80) as usize;
+                self.hram[index]
+            }
+            0xFFFF => self.ie_register,
+
+            _ => 0xFF
+        }
+    }
+
+    pub fn write_byte(&mut self, addr: u16, val: u8) {
+        match addr {
+            // Ignore write attempts to ROM
+            0x0000..=0x7FFF => {},
+            0x8000..=0x9FFF => {
+                let index = (addr - 0x8000) as usize;
+                self.vram[index] = val;
+            }
+
+            // Remove this?
+            0xC000..=0xDFFF => {
+                let index = (addr - 0xC000) as usize;
+                self.wram[index] = val;
+            }
+            0xFF80..=0xFFFE => {
+                let index = (addr - 0xFF80) as usize;
+                self.hram[index] = val;
+            }
+            0xFFFF => self.ie_register = val,
+
+            _ => {},
+        }
+    }
+
+    pub fn read_word(&self, addr: u16) -> u16 {
+        let low_byte = self.read_byte(addr) as u16;
+        let high_byte = self.read_byte(addr + 1) as u16;
+
+        (high_byte << 8) | low_byte
+    }
+
+    pub fn write_word(&mut self, addr: u16, val: u16) {
+        let low_byte = (0x00FF & val) as u8;
+        let high_byte = ((0xFF00 & val) >> 8) as u8;
+
+        self.write_byte(addr, low_byte);
+        self.write_byte(addr + 1, high_byte)
+    }
+}
